fb4ecdc1856e1bc4e46bcc74e447fdd10aae5907
Rudheus <iskandarahman1@gmail.com>
2026-03-21 20:41:25 +0700

    fix: start pakai init-db.sh

--- backend/init-db.sh
+++ backend/init-db.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 echo "Waiting for database..."
-until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER ; do
+until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER; do
   sleep 1
 done
-echo "DB_NAME is: $DB_NAME"
 echo "Database ready! Running schema..."
 PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f /app/src/db/schema.sql
-echo "Schema done. Creating default users..."
+echo "Creating default users..."
 node /app/src/db/createAdmin.js
 echo "Done! Starting server..."
-npm run dev
+node src/app.js
